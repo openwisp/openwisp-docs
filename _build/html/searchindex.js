bdce309dece34920247e06a6a42440c770d5cd8c
HargovindArora <hargovinds50@gmail.com>
2017-11-02 23:19:21 +0530

    Remove ignored files

--- _build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:50,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Newcomer&#8217;s Guide&#8217;s documentation!"],titleterms:{document:0,guid:0,indice:0,newcom:0,tabl:0,welcom:0}})
