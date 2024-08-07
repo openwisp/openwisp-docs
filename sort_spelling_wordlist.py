c7832776e4f47924fb5b441c8b969fcb3af2b5e6
Federico Capoano <f.capoano@openwisp.io>
2024-08-07 11:34:23 -0400

    [docs] Updated QA checks

--- sort_spelling_wordlist.py
+++ sort_spelling_wordlist.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 
 import argparse
-import hashlib
 import sys
-
-
-def calculate_md5(content):
-    """Calculate the MD5 hash of the given content."""
-    return hashlib.md5(content.encode('utf-8')).hexdigest()
 
 
 def sort_and_clean_file(file_path):
     """Sort, deduplicate, and clean lines in the file, then write changes if necessary."""
-    with open(file_path, 'r') as file_:
+    with open(file_path, "r") as file_:
         lines = file_.readlines()
 
     # Remove blank lines and strip leading/trailing whitespace
@@ -24,20 +18,15 @@ def sort_and_clean_file(file_path):
     # Sort lines case-insensitively but preserve their original case
     sorted_lines = sorted(unique_lines, key=str.lower)
 
-    # import ipdb; ipdb.set_trace()
-
     # Prepare content strings
-    original_content = ''.join(lines)
-    new_content = '\n'.join(sorted_lines)
+    original_content = "".join(lines)
+    new_content = "\n".join(sorted_lines)
     new_content.strip()
-    new_content += '\n'
-
-    original_md5 = calculate_md5(original_content)
-    new_md5 = calculate_md5(new_content)
+    new_content += "\n"
 
     # Write the file only if there's a change
-    if original_md5 != new_md5:
-        with open(file_path, 'w') as file:
+    if original_content != new_content:
+        with open(file_path, "w") as file:
             file.write(new_content)
         return True  # Indicates changes were made
 
@@ -46,15 +35,15 @@ def sort_and_clean_file(file_path):
 
 def main():
     """Main function to handle argument parsing and call the sorting function."""
-    parser = argparse.ArgumentParser(description='Sort and clean lines in a text file.')
+    parser = argparse.ArgumentParser(description="Sort and clean lines in a text file.")
     parser.add_argument(
-        '--qa',
-        action='store_true',
-        help='If true, script will exit with 1 if there\'s any change to spelling_wordlist.txt.',
+        "--qa",
+        action="store_true",
+        help="If true, script will exit with 1 if there's any change to spelling_wordlist.txt.",
     )
 
     args = parser.parse_args()
-    changes_made = sort_and_clean_file('./spelling_wordlist.txt')
+    changes_made = sort_and_clean_file("./spelling_wordlist.txt")
 
     if args.qa and changes_made:
         sys.exit(1)
@@ -62,5 +51,5 @@ def main():
         sys.exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
