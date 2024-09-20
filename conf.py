ddec7c2bc575a8e0416dd8bdbcb5099796460784
Federico Capoano <f.capoano@openwisp.io>
2024-09-20 18:59:23 -0400

    [qa] Minor qa change in conf.py

--- conf.py
+++ conf.py
@@ -18,8 +18,9 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
-import yaml
 from datetime import date
+
+import yaml
 
 sys.path.insert(0, os.path.abspath(os.path.curdir))
 
